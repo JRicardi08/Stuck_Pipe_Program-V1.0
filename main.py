57ccf2789170e7c20ea41be2d72f582f02b9920d
Jose Ricardi <63265557+JRicardi08@users.noreply.github.com>
2022-11-16 22:58:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import numpy as np
 
 print("\nWellcome to the stuck pipe table. Programm written by Jose Ricardi in python")
 
+# user choice
+
 choice = input(
     '\nWould you like to know the type of mechanism stuck pipe that is in the hole? "Y" or "N"\n').lower()
 
 print("\n")
 if choice == "y":
-    print("\n")
-    print("Please answer the below quiz...\n")
+    print("\nPlease answer the below quiz...\n")
 
     # Oil rig draw
 
@@ -33,106 +34,116 @@ if choice == "y":
 
     # Question 1 pior motion before sticking
 
-    move_up = input(
-        "How was the pipe moving prior the sticking? \n (a) Moving Up \n (b) Rotating Up \n (c) Moving Down \n (d) Rotating Down \n (e) Static\n ").lower()
-
     validation = ("a", "b", "c", "d", "e", "moving up", "rotating up",
                   "rotating up", "rotating down", "static", "down free", "down restricted",
                   "down impossible", "rotate free", "rotate restricted", "rotate impossible",
                   "circulation not restricted", "circulation restricted", "circulation impossible")
 
-    while move_up not in validation:
-        print("ValueError. Please enter a valid input.\n")
-        if move_up != validation:
+    table_values = ([0, 0, 0], [1, 0, 2], [0, 0, 2], [
+                    0, 2, 2], [2, 0, 0], [2, 0, 2], [2, 2, 0])
+
+    # Question 1 pior motion before sticking
+    move_up = input(
+        "Q-1 How was the pipe moving prior the sticking? \n (a) Moving Up \n (b) Rotating Up \n (c) Moving Down \n (d) Rotating Down \n (e) Static\n").lower()
+
+    def q1_moving_up():
+        global move_up
+        while move_up not in validation:
+            print("\nValueError. Please enter a valid input.\n")
             move_up = input(
-                "How was the pipe moving prior the sticking? \n (a) Moving Up \n (b) Rotating Up \n (c) Moving Down\n (d) Rotating Down \n (e) Static\n ").lower()
+                "Q-1 How was the pipe moving prior the sticking? \n (a) Moving Up \n (b) Rotating Up \n (c) Moving Down \n (d) Rotating Down \n (e) Static\n").lower()
             continue
-        break
+            break
+        if move_up == "a" or move_up == "moving up":
+            move_up = np.array((table_values[5]))
 
-    if move_up == "a" or move_up == "moving up":
-        move_up = np.array([2, 0, 2])
+        elif move_up == "b" or move_up == "rotating up":
+            move_up = np.array(table_values[2])
 
-    elif move_up == "b" or move_up == "rotating up":
-        move_up = np.array([0, 0, 2])
+        elif move_up == "c" or move_up == "moving down":
+            move_up = np.array(table_values[1])
 
-    elif move_up == "c" or move_up == "moving down":
-        move_up = np.array([1, 0, 2])
+        elif move_up == "d" or move_up == "rotaing down":
+            move_up = np.array(table_values[2])
 
-    elif move_up == "d" or move_up == "rotaing down":
-        move_up = np.array([0, 0, 2])
+        elif move_up == "e" or move_up == "static":
+            move_up = np.array(table_values[6])
 
-    elif move_up == "e" or move_up == "static":
-        move_up = np.array([2, 2, 0])
-
-    # Question 2 pipe motion after sticking
-
+    q1_moving_up()
+    
+    # Question 2 motion after sticking
     after_up = input(
-        "How was the pipe moving after the sticking? \n (a) Down Free \n (b) Down Restricted \n (c) Down Impossible\n ").lower()
-    while after_up not in validation:
-        print("ValueError. Please enter a valid input.\n")
-        if after_up != validation:
+        "Q-2 How was the pipe moving after the sticking? \n (a) Down Free \n (b) Down Restricted \n (c) Down Impossible\n").lower()
+
+    def q2_motion_after_sticking():
+        global after_up
+        while after_up not in validation:
+            print("\nValueError. Please enter a valid input.\n")
             after_up = input(
-                "How was the pipe moving after the sticking? \n (a) Down Free \n (b) Down Restricted \n (c) Down Impossible\n ").lower()
+                "Q-2 How was the pipe moving after the sticking? \n (a) Down Free \n (b) Down Restricted \n (c) Down Impossible\n").lower()
             continue
-        break
+            break
+        if after_up == "a" or after_up == "down free":
+            after_up = np.array(table_values[2])
 
-    if after_up == "a" or after_up == "down free":
-        after_up = np.array([0, 0, 2])
+        elif after_up == "b" or after_up == "down restricted":
+            after_up = np.array(table_values[1])
 
-    elif after_up == "b" or after_up == "down restricted":
-        after_up = np.array([1, 0, 2])
+        elif after_up == "c" or after_up == "down impossible":
+            after_up = np.array(table_values[0])
 
-    elif after_up == "c" or after_up == "down impossible":
-        after_up = np.array([0, 0, 0])
-
-    # Question 3 pipe rotation after sticking
-
+    q2_motion_after_sticking()
+    
+    # Question 3 rotation motion after sticking
     rotation = input(
-        "How was the pipe rotating after the sticking? \n (a) Rotate Free\n (b) Rotate Restricted\n (c) Rotate Impossible\n ").lower()
+        "Q-3 How was the pipe rotating after the sticking? \n (a) Rotate Free\n (b) Rotate Restricted\n (c) Rotate Impossible\n").lower()
 
-    while rotation not in validation:
-        print("ValueError. Please enter a valid input.\n")
-        if rotation != validation:
+    def q3_rotation_sticking():
+        global rotation
+        while rotation not in validation:
+            print("\nValueError. Please enter a valid input.\n")
             rotation = input(
-                "How was the pipe rotating after the sticking? \n (a) Rotate Free\n (b) Rotate Restricted\n (c) Rotate Impossible\n ").lower()
+                "Q-3 How was the pipe rotating after the sticking? \n (a) Rotate Free\n (b) Rotate Restricted\n (c) Rotate Impossible\n").lower()
             continue
-        break
+            break
+        if rotation == "a" or rotation == "rotate free":
+            rotation = np.array(table_values[2])
 
-    if rotation == "a" or rotation == "rotate free":
-        rotation = np.array([0, 0, 2])
+        elif rotation == "b" or rotation == "rotate restricted":
+            rotation = np.array(table_values[5])
 
-    elif rotation == "b" or rotation == "rotate restricted":
-        rotation = np.array([2, 0, 2])
+        elif rotation == "c" or rotation == "rotate impossible":
+            rotation = np.array(table_values[0])
 
-    elif rotation == "c" or rotation == "rotate impossible":
-        rotation = np.array([0, 0, 0])
-
-    # Question 4 stand pipe pressure after sticking
-
+    q3_rotation_sticking()
+    
+    # Question 4 pressure after the sticking
     circulation = input(
-        "How was the circulating pressure after the sticking? \n (a) Circulation Not Restricted\n (b) Circulation Restricted\n (c) Circulation Impossible\n ").lower()
-    while circulation not in validation:
-        print("ValueError. Please enter a valid input.\n")
-        if circulation != validation:
+        "Q-4 How was the circulating pressure after the sticking? \n (a) Circulation Not Restricted\n (b) Circulation Restricted\n (c) Circulation Impossible\n ").lower()
+
+    def q4_circulation():
+        global circulation
+        while circulation not in validation:
+            print("\nValueError. Please enter a valid input.\n")
             circulation = input(
-                "How was the circulating pressure after the sticking? \n (a) Circulation Not Restricted\n (b) Circulation Restricted\n (c) Circulation Impossible\n ").lower()
+                "Q-4 How was the circulating pressure after the sticking? \n (a) Circulation Not Restricted\n (b) Circulation Restricted\n (c) Circulation Impossible\n ").lower()
             continue
-        break
+            break
+        if circulation == "a" or circulation == "circulation not restricted":
+            circulation = np.array(table_values[3])
 
-    if circulation == "a" or circulation == "circulation not restricted":
-        circulation = np.array([0, 2, 2])
+        elif circulation == "b" or circulation == "circulation restricted":
+            circulation = np.array(table_values[6])
 
-    elif circulation == "b" or circulation == "circulation restricted":
-        circulation = np.array([2, 0, 0])
+        elif circulation == "c" or circulation == "circulation impossible":
+            circulation = np.array(table_values[0])
 
-    elif circulation == "c" or circulation == "circulation impossible":
-        circulation = np.array([0, 0, 0])
-
-    # adding the arrays for result
+    result_q4 = circulation
+    q4_circulation()
 
     result = move_up + after_up + rotation + circulation
 
-    # printing matrix result and final result
+    # Checking conditions to print type of stuck pype
 
     if result[0] > result[1] and result[0] > result[2]:
         print(
